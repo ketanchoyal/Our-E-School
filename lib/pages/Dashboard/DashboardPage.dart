15c497318e2c8646bbb55a8fcd9681171934b32d
ketanchoyal <sharmaketanr@gmail.com>
2019-06-17 12:35:04 +0530

    Optimization

--- lib/pages/Dashboard/DashboardPage.dart
+++ lib/pages/Dashboard/DashboardPage.dart
@@ -3,7 +3,6 @@ import 'package:acadamicConnect/Components/RowReusableCardButton.dart';
 import 'package:acadamicConnect/Utility/constants.dart';
 import 'package:acadamicConnect/Utility/custom_icons.dart';
 import 'package:acadamicConnect/pages/Dashboard/ResultPage.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'Announcement/AnnouncementPage.dart';
@@ -23,7 +22,7 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: EdgeInsets.all(10.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -99,27 +98,28 @@ class _DashboardState extends State<Dashboard> {
                     icon: null,
                     directionIcon: Icons.keyboard_arrow_down,
                   ),
-                  Container(
-                    height: 110,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        RowReusableCardButton(
-                          icon: FontAwesomeIcons.book,
-                          label: 'E-Book',
-                          onPressed: () {},
-                        ),
-                        RowReusableCardButton(
-                          icon: FontAwesomeIcons.cameraRetro,
-                          label: 'Video',
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ),
+                  // Container(
+                  //   height: 110,
+                  //   child: Row(
+                  //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  //     children: <Widget>[
+                  //       RowReusableCardButton(
+                  //         icon: FontAwesomeIcons.book,
+                  //         label: 'E-Book',
+                  //         onPressed: () {},
+                  //       ),
+                  //       RowReusableCardButton(
+                  //         icon: FontAwesomeIcons.cameraRetro,
+                  //         label: 'Video',
+                  //         onPressed: () {},
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
                   SizedBox(
                     height: 110,
                     child: ListView(
+                      shrinkWrap: false,
                       // padding: EdgeInsets.only(left: 10, right: 10),
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
@@ -131,6 +131,36 @@ class _DashboardState extends State<Dashboard> {
                               label: 'Exams',
                               onPressed: () {},
                             ),
+                            RowReusableCardButton(
+                          icon: FontAwesomeIcons.book,
+                          label: 'E-Book',
+                          onPressed: () {},
+                        ),
+                        RowReusableCardButton(
+                          icon: FontAwesomeIcons.cameraRetro,
+                          label: 'Video',
+                          onPressed: () {},
+                        ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 110,
+                    child: ListView(
+                      shrinkWrap: false,
+                      // padding: EdgeInsets.only(left: 10, right: 10),
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            // RowReusableCardButton(
+                            //   icon: Icons.assistant_photo,
+                            //   label: 'Exams',
+                            //   onPressed: () {},
+                            // ),
                             RowReusableCardButton(
                               icon: FontAwesomeIcons.female,
                               label: 'Parenting Guide',
