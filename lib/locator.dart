473cb994448f5c5f6c3f6c41926e102f2b07cbec
ketanchoyal <sharmaketanr@gmail.com>
2019-06-28 16:24:06 +0530

    reverted commit

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:ourESchool/core/viewmodel/HolidayModel.dart';
 import 'package:ourESchool/core/viewmodel/QuizStateModel.dart';
-
-import 'core/services/repository_calendarific.dart';
+import 'core/blocs/HolidayBlocs/repository_calendarific.dart';
 
 GetIt locator = GetIt();
 
