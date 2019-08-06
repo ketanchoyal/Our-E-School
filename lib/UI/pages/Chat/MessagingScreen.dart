96e1f139cd65347040613383ab184f4c6ac240a5
ketanchoyal <sharmaketanr@gmail.com>
2019-08-06 15:26:41 +0530

    Message Screen Send Buttom Optimized

--- lib/UI/pages/Chat/MessagingScreen.dart
+++ lib/UI/pages/Chat/MessagingScreen.dart
@@ -15,6 +15,7 @@ class MessagingScreen extends StatefulWidget {
 
 class _MessagingScreenState extends State<MessagingScreen> {
   TextEditingController _messageController;
+  bool sendButtonEnable = false;
 
   @override
   void initState() {
@@ -56,19 +57,22 @@ class _MessagingScreenState extends State<MessagingScreen> {
       to: widget.parentORteacher.id,
       for_: widget.student.id,
       from: user.id,
-      message: _messageController.text,
+      message: """${_messageController.text}""",
       timeStamp: Timestamp.now(),
     );
 
     await model.sendMessage(message: message, student: widget.student);
     _messageController.clear();
+    setState(() {
+      sendButtonEnable = false;
+    });
   }
 
   Widget _buildMessageSender(MessagingScreenPageModel model, User user) {
     return Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
         Expanded(
           child: Container(
@@ -88,6 +92,17 @@ class _MessagingScreenState extends State<MessagingScreen> {
                 child: TextField(
                   controller: _messageController,
                   keyboardType: TextInputType.multiline,
+                  onChanged: (message) {
+                    if (message.trim().length > 0) {
+                      setState(() {
+                        sendButtonEnable = true;
+                      });
+                    } else {
+                      setState(() {
+                        sendButtonEnable = false;
+                      });
+                    }
+                  },
                   // maxLength: 1000,
                   maxLines: null,
                   decoration: InputDecoration(
@@ -99,7 +114,7 @@ class _MessagingScreenState extends State<MessagingScreen> {
                         EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(
-                        Radius.circular(30.0),
+                        Radius.circular(25.0),
                       ),
                     ),
                   ),
@@ -108,41 +123,43 @@ class _MessagingScreenState extends State<MessagingScreen> {
             ),
           ),
         ),
-        Align(
-          alignment: Alignment.center,
-          child: Card(
-            margin: EdgeInsets.all(5),
-            elevation: 5,
-            shape: kCardCircularShape,
-            child: Container(
-              width: 50,
-              height: 50,
-              child: Stack(
-                children: <Widget>[
-                  model.state == ViewState.Busy
-                      ? SpinKitRing(
-                          color: Colors.red,
-                          // size: 50,
-                          lineWidth: 4,
-                        )
-                      : Container(),
-                  model.state == ViewState.Idle
-                      ? MaterialButton(
-                          height: 40,
-                          shape: kCardCircularShape,
-                          onPressed: () {
-                            sendButtonTapped(model, user);
-                          },
-                          child: Icon(
-                            Icons.send,
-                          ),
-                        )
-                      : Container(),
-                ],
-              ),
-            ),
-          ),
-        ),
+        sendButtonEnable
+            ? Align(
+                alignment: Alignment.center,
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                  elevation: 5,
+                  shape: kCardCircularShape,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    child: Stack(
+                      children: <Widget>[
+                        model.state == ViewState.Busy
+                            ? SpinKitRing(
+                                color: Colors.red,
+                                // size: 50,
+                                lineWidth: 4,
+                              )
+                            : Container(),
+                        model.state == ViewState.Idle
+                            ? MaterialButton(
+                                height: 40,
+                                shape: kCardCircularShape,
+                                onPressed: () {
+                                  sendButtonTapped(model, user);
+                                },
+                                child: Icon(
+                                  Icons.send,
+                                ),
+                              )
+                            : Container(),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            : Container(),
       ],
     );
   }
