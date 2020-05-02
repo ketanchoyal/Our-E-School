f73bd45c0559df85f8f96d4fe36700bb7d148cf2
ketanchoyal <sharmaketanr@gmail.com>
2020-05-02 13:06:14 -0400

    Code Organized"

--- lib/core/Server.dart
+++
@@ -1,9 +0,0 @@
-class Server {
-  static String baseUrl =
-      'https://us-central1-our-e-school.cloudfunctions.net/';
-  static String webApi = 'webApi/';
-  static String profileUpdate = 'profileupdate';
-  static String getProfileData = 'userdata';
-  static String postAnnouncement = 'postAnnouncement';
-  static String addAssignment = 'addAssignment';
-}
